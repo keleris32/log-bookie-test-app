82ee37ee27c52540c72e7b83d53ddc12d8c0aa0e
keleris32 <charles.ejiegbu@gmail.com>
2022-06-19 09:00:23 +0100

    feat: added document to project

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta property="og:site_name" content="Expanzo" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+          <link rel="icon" href="/favicon.png" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
